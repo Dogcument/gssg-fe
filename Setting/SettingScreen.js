6564fe50b54106d15e632e8ef6d24427c703c01a
gunny <ohhh123@naver.com>
2021-01-29 18:26:30 +0900

    Setting탭에 Drawer navigation 추가

--- Setting/SettingScreen.js
+++ Setting/SettingScreen.js
@@ -1,12 +1,26 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// Gunny TODO
+// Tempcode
+// screen component (in Setting, it would be etc)
+import WritingScreen from '../Writing/WritingScreen';
+import StorageScreen from '../Storage/StorageScreen';
+
+const Drawer = createDrawerNavigator();
 
 function SettingScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Storage Screen</Text>
-      </View>
-    );
-  }
-  
+  return (
+    <NavigationContainer independent={true}>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Writing" component={WritingScreen}></Drawer.Screen>
+        <Drawer.Screen name="Storage" component={StorageScreen}></Drawer.Screen>
+        <Drawer.Screen name="Setting" component={SettingScreen}></Drawer.Screen>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default SettingScreen; 
