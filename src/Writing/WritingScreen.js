5b51c8e9b47c0435f28d50c4211bd1bf5a54c61e
gunny <ohhh123@naver.com>
2021-02-06 17:26:43 +0900

    WritingScreen에 작성할 수 있도록 TextInput 컴포넌트 사용

--- src/Writing/WritingScreen.js
+++ src/Writing/WritingScreen.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput } from 'react-native';
 
 function WritingScreen() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>실제로 작성하는 곳</Text>
+        <TextInput>실제로 작성하는 곳</TextInput>
       </View>
     );
   }
