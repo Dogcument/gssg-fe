38caa4d1aaf92e37cdae39811b319f87d063b932
gunny <ohhh123@naver.com>
2021-02-09 22:51:39 +0900

    지웅이 코드 이전
    - _Style폴더 생성
    
    - ScrollView 추가

--- src/Cabinet/CabinetScreen.js
+++ src/Cabinet/CabinetScreen.js
@@ -1,12 +1,63 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import styles from '../_Style/Styles';
 
 function CabinetScreen() {
-    return (
+  return (
+    <ScrollView>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Cabinet Screen</Text>
       </View>
-    );
-  }
-  
+      <View style={styles.container}>
+
+        <View style={styles.box}>
+          <View style={styles.contents}>
+            <Text style={styles.boxText}>
+              contents
+            </Text>
+          </View>
+          <View style={styles.profile}>
+            <Text style={styles.boxText}>
+              profile
+            </Text>
+          </View>
+        </View>
+        <View style={styles.blank} />
+
+        <View style={styles.box}>
+          <View style={styles.contents}>
+            <Text style={styles.boxText}>
+              contents
+            </Text>
+          </View>
+          <View style={styles.profile}>
+            <Text style={styles.boxText}>
+              profile
+            </Text>
+          </View>
+        </View>
+        <View style={styles.blank} />
+
+
+        <View style={styles.box}>
+          <View style={styles.contents}>
+            <Text style={styles.boxText}>
+              contents
+            </Text>
+          </View>
+          <View style={styles.profile}>
+            <Text style={styles.boxText}>
+              profile
+            </Text>
+          </View>
+        </View>
+        <View style={styles.blank} />
+
+      </View>
+
+    </ScrollView>
+  )
+}
+
 export default CabinetScreen; 
---
+++ src/_Style/Styles.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+
+    box: {
+        height: 200,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'left',
+        alignSelf: 'center',
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        borderColor: '#110000'
+    },
+
+    contents: {
+        height: '100%',
+        flex: 0.75,
+        flexDirection: 'row',
+        backgroundColor: '#00ff00'
+    },
+
+    profile: {
+        height: '100%',
+        flex: 0.25,
+        flexDirection: 'row',
+        backgroundColor: '#fff'
+    },
+
+    blank: {
+        height: 30,
+        width: '100%'
+    },
+
+    boxText: {
+        color: '#000',
+        fontWeight: 'bold'
+    }
+});
+
+export default styles;
