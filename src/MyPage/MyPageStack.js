aa80debc5953c5173156ea74f6fea5f949de4c09
gunny <ohhh123@naver.com>
2021-03-14 14:06:30 +0900

    세팅 화면 추가

--- src/MyPage/MyPageStack.js
+++ src/MyPage/MyPageStack.js
@@ -3,15 +3,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useNavigation } from "@react-navigation/native";
 import MyPageScreen from './MyPageScreen';
+import { SettingScreen } from './SettingScreen';
 
 const Stack = createStackNavigator();
 
-function MyPageStack() {
+function MyPageStack({ navigation }) {
     return (
         <Stack.Navigator>
             <Stack.Screen
                 name="MyPage"
-                component={() => <MyPageScreen navigation={useNavigation()} />}
+                component={() => <MyPageScreen navigation={navigation} />}
                 options={{
                     title: "마이페이지",
                     headerTitleAlign: 'left',
@@ -19,18 +20,30 @@ function MyPageStack() {
                         <HeaderButtons>
                             <Item
                                 title="종"
-                                onPress={() => alert("종")}
+                                onPress={() => alert("개발중입니다.")}
                             />
                             <Item
                                 title="톱니"
-                                onPress={() => alert("톱니")}
+                                onPress={() => OpenSettingScreen(navigation)}
                             />
                         </HeaderButtons>
                     )
                 }}
             />
+            <Stack.Screen
+                name="Setting"
+                component={() => <SettingScreen />}
+                options={{
+                    title: "세팅",
+                    headerTitleAlign: 'left'
+                }}>
+            </Stack.Screen>
         </Stack.Navigator>
     );
+}
+
+function OpenSettingScreen(navigation) {
+    navigation.navigate('Setting');
 }
 
 export default MyPageStack;
---
+++ src/MyPage/SettingScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+export class SettingScreen extends React.Component {
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
