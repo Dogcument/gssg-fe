56c102a1d4adabeaaa731ce38a756526fdefa296
gunny <ohhh123@naver.com>
2021-03-28 12:12:44 +0900

    네비게이션 바 이미지 추가

--- src/Main/MainScreen.js
+++ src/Main/MainScreen.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
 
 // screen component
 import WritingStack from '../Writing/WritingStack';
 import CabinetStack from '../Cabinet/CabinetStack';
 import MyPageStack from '../MyPage/MyPageStack';
+
+import { navigationIcons } from './TabBarIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,9 +16,48 @@ function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="글쓰기" component={WritingStack}></Tab.Screen>
-        <Tab.Screen name="보관함" component={CabinetStack}></Tab.Screen>
-        <Tab.Screen name="마이페이지" component={MyPageStack}></Tab.Screen>
+        <Tab.Screen
+          name="글쓰기"
+          component={WritingStack}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <Image
+                  source={focused ? navigationIcons[0].active : navigationIcons[0].inactive}
+                  style={{ width: 20, height: 20 }}
+                />
+              );
+            }
+          }}
+        />
+        <Tab.Screen
+          name="보관함"
+          component={CabinetStack}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <Image
+                  source={focused ? navigationIcons[1].active : navigationIcons[1].inactive}
+                  style={{ width: 20, height: 20 }}
+                />
+              );
+            }
+          }}
+        />
+        <Tab.Screen
+          name="마이페이지"
+          component={MyPageStack}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <Image
+                  source={focused ? navigationIcons[2].active : navigationIcons[2].inactive}
+                  style={{ width: 20, height: 20 }}
+                />
+              );
+            }
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
