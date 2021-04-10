9f9d5d2f96c2e8eb45d9bd67cbcd24d015b1fe3a
gunny <ohhh123@naver.com>
2021-04-10 11:33:43 +0900

    MyPageItem에서 시간을 YYYY.MM.DD HH:MM 포멧으로 출력하도록 수정

--- src/MyPage/MyPageItem.js
+++ src/MyPage/MyPageItem.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import { styles } from './Styles';
+import moment from 'moment';
 
 export class MyPageItem extends React.Component {
   render() {
-    const writingTime = this.props.writingTime;
+    const writingTimeEpoch = Number(this.props.writingTime);
+    const writingTime = moment(writingTimeEpoch).format('YYYY.MM.DD HH:MM');
     const content = this.props.content;
     const navigation = this.props.navigation;
     return (
