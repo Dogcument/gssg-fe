1f1b84b055a3d588e21e97f0ac2255ece139b5cb
Jiwung Lim <jiwung1994@gmail.com>
2021-04-17 14:42:53 +0900

    Popup 디자인

--- src/Logo/Popup.js
+++ src/Logo/Popup.js
@@ -1,28 +1,27 @@
 import React from "react";
-import { render } from "react-dom";
 import {
   Text,
   View,
   Modal,
   TouchableHighlight,
   StyleSheet,
-  Dimensions
+  Image
 } from "react-native";
-import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { TextInput } from "react-native-gesture-handler";
 
 const Popup = props => {
   return (
     <Modal animationType="fade" transparent={true} visible={props.visible}>
       <View style={styles.modalBackground}>
-        <View style={{ margin: 5, flex : 1 }}>
-          <View style={{ flexDirection: 'row', flex: 1, justifyContent : 'center' }}>
-            <View style={{flex: 0.5, backgroundColor: '#000000', marginLeft : 5, marginRight : 10}}>
-            </View>
-            <Text style={{ flex: 8, fontFamily: 'SpoqaBold', fontSize: 20 }}>
+        <View style={{ margin: 15, flex : 1 }}>
+          <View style={{ flexDirection: 'row', flex: 1, justifyContent : 'center', alignItems : 'center' }}>
+            <View style={{backgroundColor : '#000000', flex : 0.5}}/>
+            <Text style={{ flex: 8, fontFamily: 'SpoqaBold', fontSize: 20, marginLeft : 10 }}>
               작가 등록증
             </Text>
-            <TouchableHighlight style={{ flex: 0.5 }} onPress={() => props.setPopupVisible(false)}>
-              <Text>-></Text>
+            <TouchableHighlight style={{ flex: 0.5, marginRight : 10 }} onPress={() => props.setPopupVisible(false)}>
+              <Image style={{width : 20, height : 20}}
+                source={require('../Main/Images/BackButton.png')} />
             </TouchableHighlight>
           </View>
 
@@ -30,15 +29,13 @@ const Popup = props => {
             <Text style={{ fontFamily: 'SpoqaMedium' }}>
               필명
             </Text>
-            <TextInput style={{ backgroundColor: '#FFFFFF', borderRadius: 5, width: '60%' }}>
-
-            </TextInput>
+            <TextInput placeholder="필명을 입력해주세요!"
+              style={{ fontSize : 12, backgroundColor: '#d4d4d4', borderRadius: 5, width: '60%', height : '23%' }} />
             <Text style={{ fontFamily: 'SpoqaMedium' }}>
               한 줄 소개
             </Text>
-            <TextInput style={{ backgroundColor: '#FFFFFF', borderRadius: 5, width: '60%' }}>
-
-            </TextInput>
+            <TextInput placeholder="간단한 설명을 해주세요!"
+              style={{ fontSize : 12, backgroundColor: '#d4d4d4', borderRadius: 5, width: '100%', height : '23%' }} />
           </View>
         </View>
       </View>
@@ -51,11 +48,9 @@ const styles = StyleSheet.create({
   modalBackground: {
     width: '80%',
     height: '20%',
-    backgroundColor: "#70F2BD",
-  },
-  modal: {
-    backgroundColor: '#ae9784',
-    width: "80%",
-    height: "20%"
+    backgroundColor : '#FFFFFF',
+    borderColor : '#d4d4d4',
+    marginTop : '120%', marginLeft : '10%',
+    borderRadius : 10
   }
 });
