f5e0d565b0b07ef462ae7ef03ca7a4fbc1ea419c
Jiwung Lim <jiwung1994@gmail.com>
2021-04-25 10:42:40 +0900

    Popup 강아지 선택화면 추가

---
+++ src/Logo/Popup 필명.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  Text,
+  View,
+  Modal,
+  TouchableHighlight,
+  StyleSheet,
+  Image
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+const Popup = props => {
+  return (
+    <Modal animationType="fade" transparent={true} visible={props.visible}>
+      <View style={styles.modalBackground}>
+        <View style={{ margin: 15, flex : 1 }}>
+          <View style={{ flexDirection: 'row', flex: 1, justifyContent : 'center', alignItems : 'center' }}>
+            <Image source={require('../Writing/Images/1_WritingButton.png')} style={{ height : 25, width : 25 }} />
+            <Text style={{ flex: 8, fontFamily: 'SpoqaBold', fontSize: 20, marginLeft : 10 }}>
+              작가 등록증
+            </Text>
+            <TouchableHighlight style={{ flex: 0.5, marginRight : 10 }} onPress={() => props.setPopupVisible(false)}>
+              <Image style={{width : 25, height : 25}}
+                source={require('../Main/Images/NextButton.png')} />
+            </TouchableHighlight>
+          </View>
+
+          <View style={{ flex: 4, flexDirection: 'column', justifyContent: 'space-around', marginTop : 10 }}>
+            <Text style={{ fontFamily: 'SpoqaMedium' }}>
+              필명
+            </Text>
+            <TextInput placeholder="필명을 입력해주세요!" placeholderTextColor='#FFFFFF'
+              style={{ fontSize : 12, backgroundColor: '#d4d4d4', borderRadius: 5, width: '60%', height : '23%', paddingLeft : 5 }} />
+            <View style={{height : 5}}></View>
+            <Text style={{ fontFamily: 'SpoqaMedium' }}>
+              한 줄 소개
+            </Text>
+            <TextInput placeholder="간단한 설명을 해주세요!" placeholderTextColor='#FFFFFF'
+              style={{ fontSize : 12, backgroundColor: '#d4d4d4', borderRadius: 5, width: '100%', height : '23%', paddingLeft : 5 }} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+export default Popup;
+
+const styles = StyleSheet.create({
+  modalBackground: {
+    width: '80%',
+    height: '20%',
+    backgroundColor : '#FFFFFF',
+    borderColor : '#d4d4d4',
+    marginTop : '120%', marginLeft : '10%',
+    borderRadius : 10
+  }
+});
--- src/Logo/Popup.js
+++ src/Logo/Popup.js
@@ -2,40 +2,61 @@ import React from "react";
 import {
   Text,
   View,
+  ScrollView,
   Modal,
   TouchableHighlight,
   StyleSheet,
   Image
 } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Popup = props => {
   return (
     <Modal animationType="fade" transparent={true} visible={props.visible}>
       <View style={styles.modalBackground}>
-        <View style={{ margin: 15, flex : 1 }}>
-          <View style={{ flexDirection: 'row', flex: 1, justifyContent : 'center', alignItems : 'center' }}>
-            <View style={{backgroundColor : '#000000', flex : 0.5}}/>
-            <Text style={{ flex: 8, fontFamily: 'SpoqaBold', fontSize: 20, marginLeft : 10 }}>
-              작가 등록증
-            </Text>
-            <TouchableHighlight style={{ flex: 0.5, marginRight : 10 }} onPress={() => props.setPopupVisible(false)}>
-              <Image style={{width : 20, height : 20}}
-                source={require('../Main/Images/BackButton.png')} />
-            </TouchableHighlight>
-          </View>
+        <View style={{ margin: 15, flex: 1, flexDirection: 'row' }}>
+          <ScrollView horizontal={false}
+            style={{ width: '65%', flexDirection: 'column', backgroundColor: '#a98baa' }}>
+            <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around', padding: 5 }}>
+              <TouchableOpacity>
+                <Image source={require('../MyPage/Images/기본강아지.png')}
+                  style={{ width: 45, height: 45 }} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image source={require('../MyPage/Images/불독.png')}
+                  style={{ width: 45, height: 45 }} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image source={require('../MyPage/Images/사모예드.png')}
+                  style={{ width: 45, height: 45 }} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image source={require('../MyPage/Images/요크.png')}
+                  style={{ width: 45, height: 45 }} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image source={require('../MyPage/Images/코기.png')}
+                  style={{ width: 45, height: 45 }} />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Image source={require('../MyPage/Images/허스키.png')}
+                  style={{ width: 45, height: 45 }} />
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
 
-          <View style={{ flex: 4, flexDirection: 'column', justifyContent: 'space-around', marginTop : 10 }}>
-            <Text style={{ fontFamily: 'SpoqaMedium' }}>
-              필명
-            </Text>
-            <TextInput placeholder="필명을 입력해주세요!"
-              style={{ fontSize : 12, backgroundColor: '#d4d4d4', borderRadius: 5, width: '60%', height : '23%' }} />
-            <Text style={{ fontFamily: 'SpoqaMedium' }}>
-              한 줄 소개
-            </Text>
-            <TextInput placeholder="간단한 설명을 해주세요!"
-              style={{ fontSize : 12, backgroundColor: '#d4d4d4', borderRadius: 5, width: '100%', height : '23%' }} />
+          <View style={{ flexDirection: 'column', width: '35%' }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <TouchableHighlight style={{ marginRight: 10 }} onPress={() => props.setPopupVisible(false)}>
+                <Image style={{ width: 25, height: 25 }}
+                  source={require('../Main/Images/NextButton.png')} />
+              </TouchableHighlight>
+            </View>
+            <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', marginTop : -15 }}>
+              <Image source={require('../MyPage/Images/기본강아지.png')}
+                style={{ width: 80, height: 80 }} />
+              <Text style={{ fontFamily: 'SpoqaBold', fontSize: 15 }}> 강아지 </Text>
+            </View>
           </View>
         </View>
       </View>
@@ -48,9 +69,9 @@ const styles = StyleSheet.create({
   modalBackground: {
     width: '80%',
     height: '20%',
-    backgroundColor : '#FFFFFF',
-    borderColor : '#d4d4d4',
-    marginTop : '120%', marginLeft : '10%',
-    borderRadius : 10
+    backgroundColor: '#FFFFFF',
+    borderColor: '#d4d4d4',
+    marginTop: '120%', marginLeft: '10%',
+    borderRadius: 10
   }
 });
