5ebab8b351bbf7261e2de5cf70723db6b1a5f1f6
gunny <ohhh123@naver.com>
2021-05-02 15:13:15 +0900

    튜토리얼 클릭시 다음 튜토리얼로, 그리고  튜토리얼 마무리 되면 Main화면으로 넘어가도록 수정

--- src/SignUp/TutorialScreen.js
+++ src/SignUp/TutorialScreen.js
@@ -16,20 +16,41 @@ const Images = [
     require('./Images/Pop_up_6.png')
 ]
 
-export const TutorialScreen = props => {
-    return (
-        <View style={{ width: '100%', height: '100%' }}>
-            <ImageBackground source={require('../Logo/Images/1_Logo.png')} style={{ position: 'absolute', width: '100%', height: '100%' }} />
-            <View style={{ width : '100%', height : '100%', backgroundColor : '#000000', opacity : 0.5}} />
-            <View style={styles.tutorialBackground}>
-                <TouchableOpacity style={{ height : '100%', width : '100%' }}>
-                    <Image style={styles.popupImage}
-                        source={Images[0]}>
-                    </Image>
-                </TouchableOpacity>
+const MAX_IMAGE_NUM = 5;
+export class TutorialScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageIndex: 0
+        }
+    }
+
+    ShowNextTutorial() {
+        if (MAX_IMAGE_NUM == this.state.imageIndex) {
+            this.props.GotoMainScreen();
+            return;
+        }
+
+        this.setState({ imageIndex: this.state.imageIndex + 1 });
+    }
+
+    render() {
+        return (
+            <View style={{ width: '100%', height: '100%' }}>
+                <ImageBackground source={require('../Logo/Images/1_Logo.png')} style={{ position: 'absolute', width: '100%', height: '100%' }} />
+                <View style={{ width: '100%', height: '100%', backgroundColor: '#000000', opacity: 0.5 }} />
+                <View style={styles.tutorialBackground}>
+                    <TouchableOpacity
+                        style={{ height: '100%', width: '100%' }}
+                        onPress={() => this.ShowNextTutorial()}>
+                        <Image style={styles.popupImage}
+                            source={Images[this.state.imageIndex]}>
+                        </Image>
+                    </TouchableOpacity>
+                </View>
             </View>
-        </View>
-    );
+        );
+    }
 };
 
 const styles = StyleSheet.create({
@@ -43,18 +64,18 @@ const styles = StyleSheet.create({
         borderRadius: 10
     },
     tutorialBackground: {
-        position : 'absolute',
+        position: 'absolute',
         width: '80%',
-        height : '65%',
+        height: '65%',
         resizeMode: 'contain',
         backgroundColor: '#FFFFFF',
         borderColor: '#d4d4d4',
         marginTop: '32.5%', marginLeft: '10%',
         borderRadius: 10
     },
-    popupImage : {
+    popupImage: {
         resizeMode: 'contain',
-        width : '100%',
-        height : '100%'
+        width: '100%',
+        height: '100%'
     }
 });
