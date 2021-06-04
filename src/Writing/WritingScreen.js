cae64b41977385e1cf125471d1ad9a2095b88208
gunny <ohhh123@naver.com>
2021-06-04 16:11:17 +0900

    WritingScreen에서 빈 화면 클릭하면 키보드 숨기기

--- src/Writing/WritingScreen.js
+++ src/Writing/WritingScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import { styles } from './Styles';
 
@@ -7,22 +7,24 @@ var content = "";
 var subject = "";
 
 export class WritingScreen extends React.Component {
-  componentDidMount = async() => {
+  componentDidMount = async () => {
     subject = this.props.subject;
     this.props.navigation.setOptions({ title: subject });
   }
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '95%', marginLeft: '2.5%' }}>
-        <TextInput
-          style={styles.TextInputStyle}
-          placeholder="여기에 입력"
-          multiline={true}
-          returnKeyType='default'
-          onChangeText={inputText => this.onChangeText(inputText)}
-        />
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '95%', marginLeft: '2.5%' }}>
+          <TextInput
+            style={styles.TextInputStyle}
+            placeholder="여기에 입력"
+            multiline={true}
+            returnKeyType='default'
+            onChangeText={inputText => this.onChangeText(inputText)}
+          />
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 
