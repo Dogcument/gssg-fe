150529aef1151c2cd110063c9882b75f627ff162
gunny <ohhh123@naver.com>
2021-07-01 23:57:47 +0900

    Revert "AuthScreen 내 SignIn Modal 추가"
    This reverts commit d770376eace429d384b20b1264445c8922e75d7d.

--- src/Auth/AuthScreen.js
+++ src/Auth/AuthScreen.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { View, ImageBackground, Text, TouchableOpacity, TextInput } from "react-native";
-import { Modal } from "react-native-modal";
+import { ImageBackground } from "react-native";
 import * as Font from "expo-font";
 import { SignUpScreen } from "./SignUpScreen";
-import { SignInScreen } from "./SignInScreen";
 import MainScreen from "../Main/MainScreen";
 import AsyncStorage from "@react-native-community/async-storage";
 import { LogoImg } from "../../assets/ImageList";
 import UserInfo from "../Common/UserInfo";
-import { styles } from "./Styles";
 
 export const SignUpState = {
   SetNickname: 1,
@@ -23,8 +20,6 @@ export default class AuthScreen extends React.Component {
       goMainScreen: false,
       isLoading: true,
       hasSession: false,
-      //Modal state
-      visibleModal: null,
 
       // SignUp state
       signUpState: SignUpState.SetNickname,
@@ -66,49 +61,6 @@ export default class AuthScreen extends React.Component {
     this.setState({ isLoading: false });
   };
 
-  RenderCloseButton = (text, onPress) => (
-    <TouchableOpacity onPress={onPress} style={[styles.modalbutton]}>
-      <View>
-        <Text style={{ fontFamily: "SpoqaBold", fontSize: 20 }}> Sign in </Text>
-      </View>
-    </TouchableOpacity>
-  );
-  RenderSignIn() {
-    return (
-      <View style={[styles.SignInModal]}>
-        <Text>이메일</Text>
-        <TextInput
-          placeholder="이메일을 입력해주세요!"
-          placeholderTextColor="#FFFFFF"
-          style={{
-            fontSize: 12,
-            backgroundColor: "#d4d4d4",
-            borderRadius: 5,
-            width: "60%",
-            height: "23%",
-            paddingLeft: 5,
-          }}
-        />
-        <Text>비밀번호</Text>
-        <TextInput
-          placeholder="비밀번호를 입력해주세요!"
-          placeholderTextColor="#FFFFFF"
-          style={{
-            fontSize: 12,
-            backgroundColor: "#d4d4d4",
-            borderRadius: 5,
-            width: "60%",
-            height: "23%",
-            paddingLeft: 5,
-          }}
-        />
-        {this.RenderCloseButton("닫기", () =>
-          this.setState({ visibleModal: null })
-        )}
-      </View>
-    );
-  }
-
   // TODO : EncryptedStorage does not support expo :(
   // retrieveUserSession = async () => {
   //   try {
@@ -133,12 +85,10 @@ export default class AuthScreen extends React.Component {
 
     if (this.state.isLoading) {
       return (
-        <View style={{ flexDirection: "row", width: "100%", height: "100%" }}>
-          <ImageBackground
-            source={LogoImg}
-            style={{ position: "absolute", width: "100%", height: "100%" }}
-          />
-        </View>
+        <ImageBackground
+          source={LogoImg}
+          style={{ position: "absolute", width: "100%", height: "100%" }}
+        />
       );
     } else {
       if (this.state.hasSession) {
@@ -152,26 +102,7 @@ export default class AuthScreen extends React.Component {
 
         // Tempcode
         return (
-          <View
-            style={{ flexDirection: "column", width: "100%", height: "100%" }}
-          >
-            <ImageBackground
-              source={LogoImg}
-              style={{ position: "absolute", width: "100%", height: "100%" }}
-            />
-            <View style={{ height: "70%" }} />
-            <View>
-              {this.RenderCloseButton("Sign In", () =>
-                this.setState({ visibleModal: 1 })
-              )}
-            </View>
-
-            <Modal isVisible={this.state.visibleModal === 1}>
-              {this.RenderSignIn()}
-            </Modal>
-          </View>
-
-          //<SignUpScreen signUp={true} GotoMainScreen={this.GotoMainScreen} />
+          <SignUpScreen signUp={true} GotoMainScreen={this.GotoMainScreen} />
         );
       }
     }
