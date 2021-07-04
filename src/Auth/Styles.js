37e5e977af0f84c30b3d5e3edc01139630d01a06
gunny <ohhh123@naver.com>
2021-07-04 11:50:32 +0900

    Revert "Revert "AuthScreen 내 SignInScreen UI 작업 /inVision 참고""
    This reverts commit 43ed23f129b0ffbe33311982fd9512bf73ed33e8.

--- src/Auth/Styles.js
+++ src/Auth/Styles.js
@@ -3,10 +3,16 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     SignInModal: {
         flexDirection: 'column',
+<<<<<<< HEAD
         width: '90%',
         height: '25%',
         marginTop: '130%',
         marginLeft: '5%',
+=======
+        width: '80%',
+        height: '30%',
+        marginTop: '60%',
+>>>>>>> 87ac607d3daf2a11afeb481d617da43f441f317d
         padding: 20,
         borderRadius: 5,
         backgroundColor: '#FFFFFF',
